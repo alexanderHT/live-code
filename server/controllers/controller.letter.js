ea635da1a5505a0616285cdd922c01065f45e928
Alexander Hendrawan tjahja <alexanderhendrawan@gmail.com>
2017-02-23 14:20:14 +0700

    create view letter

--- server/controllers/controller.letter.js
+++ server/controllers/controller.letter.js
@@ -9,17 +9,38 @@ var controllerLetter = {
     })
   },
   /* search letter and frequency */
-  searchLetter: function (req, res) {},
+  searchLetter: function (req, res) {
+    console.log(req.body)
+    let letter_input = req.body.letter || ''
+    let frequency_input = Number(req.body.frequency) || 0
+
+    if (letter_input && frequency_input) {
+      modelLetter.find({ letter: letter_input, frequency: frequency_input }, function (err, data) {
+        if (err) console.log(err)
+        res.json(data)
+      })
+    }else {
+      modelLetter.find({}).or([{ letter: letter_input }, { frequency: frequency_input }]).exec(function (err, data) {
+        if (err) console.log(err)
+        res.json(data)
+      })
+    }
+  },
   /* create one Letter to database */
   addOneLetter: function (req, res) {
     let newLetter = modelLetter({
       letter: req.body.letter,
       frequency: req.body.frequency
     })
+
+    newLetter.save(function (err, data) {
+      if (err) throw err
+      res.json(data)
+    })
   },
   /* edit one letter from database */
   editOneLetter: function (req, res) {
-    modelLetter.findOneAndUpdate({ _id: req.body.id }, { letter: req.body.letter, frequency: req.body.frequency }, function (err, data) {
+    modelLetter.findOneAndUpdate({ _id: req.body.id }, { letter: req.body.letter, frequency: req.body.frequency }, { new: true } , function (err, data) {
       if (err) throw err
       res.json(data)
     })
@@ -32,7 +53,60 @@ var controllerLetter = {
     })
   },
   /* insert many document to database */
-  seederLetter: function (req, res) {}
+  seederLetter: function (req, res) {
+    var arrayLetter =
+    [{
+      letter: 'a',
+      frequency: 1
+    }, {
+      letter: 'a',
+      frequency: 2
+    }, {
+      letter: 'a',
+      frequency: 3
+    }, {
+      letter: 'a',
+      frequency: 4
+    }, {
+      letter: 'a',
+      frequency: 5
+    }, {
+      letter: 'b',
+      frequency: 1
+    }, {
+      letter: 'b',
+      frequency: 2
+    }, {
+      letter: 'b',
+      frequency: 3
+    }, {
+      letter: 'b',
+      frequency: 4
+    }, {
+      letter: 'c',
+      frequency: 1
+    }, {
+      letter: 'c',
+      frequency: 2
+    }, {
+      letter: 'c',
+      frequency: 3
+    }, {
+      letter: 'c',
+      frequency: 4
+    }]
+    modelLetter.collection.insert(arrayLetter, function (err, data) {
+      if (err) throw err
+      let temp = data.ops
+      res.json(temp)
+    })
+  },
+  removeLetter: function (req, res) {
+    modelLetter.remove({}, function (err, data) {
+      if (err) throw err
+      console.log(' Data hes been deleted : ' + data)
+    })
+  }
 }
 
 module.exports = controllerLetter
--- server/routes/letters.js
+++ server/routes/letters.js
@@ -4,15 +4,15 @@ const controllerLetter = require('../controllers/controller.letter.js')
 
 /* http://localhost:3000/api/letters | get all data letter from database */
 router.get('/', controllerLetter.getAllLetter)
-/* http://localhost:3000/api/letters | search letter and frequency */
-router.get('/search', controllerLetter.searchLetter)
+/* http://localhost:3000/api/letters/search | search letter and frequency */
+router.post('/search', controllerLetter.searchLetter)
 /* http://localhost:3000/api/letters | create one Letter to database */
 router.post('/', controllerLetter.addOneLetter)
 /* http://localhost:3000/api/letters | edit one letter from database */
 router.put('/', controllerLetter.editOneLetter)
 /* http://localhost:3000/api/letters | delete one letter from database */
 router.delete('/', controllerLetter.deleteOneLetter)
-/* http://localhost:3000/api/letters | insert many document to database */
+/* http://localhost:3000/api/letters/seederLetter | insert many document to database */
 router.get('/seederLetter', controllerLetter.seederLetter)
 
 module.exports = router
